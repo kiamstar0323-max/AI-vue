d33f318feb91ef11326a616ffd59ec65cdae7dfc
kiamsatr <3586236624@qq.com>
2026-03-25 18:18:14 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://159.75.169.224:1235',
+        changeOrigin: true
+      }
+    }
+  }
+})
